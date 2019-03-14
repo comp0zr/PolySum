959b9ede6f8632a3c6e5a3ffabe49d49e73e694e
Fabio Cardeal <ficarde@gmail.com>
2019-03-14 15:36:43 -0300

    documentation, removing extraneous folders, removing git tracking from sub-folders, simplifying python implementation

--- Python/polysum.py
+++ Python/polysum.py
@@ -3,24 +3,44 @@ import sympy as sp
 from fractions import Fraction
 from collections import namedtuple
 
-
-PolySumValues = namedtuple('PolySumValues', ('zero', 'one', 'dtype'))
-
-
-VALUES = {
-    'fraction': PolySumValues(Fraction(0,1), Fraction(1,1), 'object'),
-    'float64' : PolySumValues(np.float64(0), np.float64(1), 'float64'),
-    'symbolic': PolySumValues(sp.sympify(0), sp.sympify(1), 'object'),
-}
+ONE   = sp.sympify(1)
+ZERO  = sp.sympify(0)
+DTYPE = 'object'
 
 DEFAULT_SYM = sp.symbols('x')
-DEFAULT_VTYPE = 'symbolic'
 
 
-def prepare_matrix(n: int, v):
-    A = np.full((n+1,n+2), v.zero, v.dtype)
-    A[0] = np.full(n+2, v.one, v.dtype)
-    #
+def prepare_matrix(n):
+    """
+    returns a matrix A where each element A[i,j] has three possible values:
+      if j < i, then A[i, j] = 0
+      if j = n+1, then A[i, j] = n!/(n - i + 1)!
+      otherwise, A[i, j] = j!(j - i + 1)!
+
+    n is assumed to be an integer.
+    """
+    # initialize a matrix of zeros, then fill first line with ones
+    A = np.full((n+1,n+2), ZERO, DTYPE)
+    A[0] = np.full(n+2, ONE, DTYPE)
+    # use each line to calculate the next one
+    for i in range(1, n+1):
+        # ignore all elements where j < i, since we know they are zero
+        for j in range(i, n+1):
+            # this simple multiplication is all we need,
+            # since A[i, j] = j!/(j - i + 1)!, and since
+            # we already calculated A[i-1, j] = j!/(j - i + 2)!
+            A[i,j] = A[i-1,j] * (j - i + 2)
+        # the last column is the same as the antepenultimate column...
+        A[i,n+1] = A[i,n-1]
+    # ...with the exception of A[n, n+1],
+    # which repeats last element of the previous line.
+    A[n,n+1] = A[n-1,n+1]
+    return A
+
+# uncommentated
+def _prepare_matrix(n):
+    A = np.full((n+1,n+2), ZERO, DTYPE)
+    A[0] = np.full(n+2, ONE, DTYPE)
     for i in range(1, n+1):
         for j in range(i, n+1):
             A[i,j] = A[i-1,j] * (j - i + 2)
@@ -29,41 +49,75 @@ def prepare_matrix(n: int, v):
     return A
 
 
-def gauss_jordan(A: np.ndarray, v):
+def gauss_jordan(A):
+    """
+    performs an in-place partial gauss-jordan elimination on A,
+    assuming it is already in row echelon form, and turning it
+    into its reduced row echelon form.
+    """
     n = len(A) - 1
     for i in range(n, -1, -1):
         A[i,n+1] /= A[i,i]
-        A[i,i] = v.one
+        A[i,i] = ONE
         for j in range(i-1, -1, -1):
             p = A[j,i]
-            A[j,i] = v.zero
+            A[j,i] = ZERO
             A[j, n+1] -= A[i, n+1] * p
     return A
 
 
-def polysum_coefficients(n: int, vtype=DEFAULT_VTYPE):
-    v = VALUES.get(vtype, vtype)
-    A = gauss_jordan(prepare_matrix(n, v), v)
+def polysum_coefficients(n):
+    """
+    obtains the coefficients C of the polynomial Q such
+    that Q(x) = summation(i^n, (i, 0, x)), where C[k]
+    is the coefficient of the term x^(k+1) in Q(x).
+
+    assumes n is an integer.
+    """
+    A = gauss_jordan(prepare_matrix(n))
     return A[:, n+1]
 
 
-def polysum_n(n: int, vtype=DEFAULT_VTYPE, x=DEFAULT_SYM):
-    coeffs = polysum_coefficients(n, vtype)
-    return sum(coeffs[i-1] * x**i for i in range(1,n+2))
+def polysum_n(n, variable=DEFAULT_SYM):
+    """
+    returns the polynomial Q in terms of the given variable such
+    that Q(x) = summation(i^n, (i,0,x)).
+
+    assumes n is an integer, and the variable defaults to DEFAULT_SYM.
+    """
+    coeffs = polysum_coefficients(n)
+    return sum(coeffs[i-1] * variable**i for i in range(1,n+2))
 
 
-def polysum_sym(expr, vtype=DEFAULT_VTYPE, x=None):
-    if x == None and len(expr.free_symbols) == 1:
-      x = tuple(expr.free_symbols)[0]
-    #
-    poly_terms = expr.as_poly().as_dict().items()
-    return sum(c * polysum_n(n, vtype, x) for ((n,), c) in poly_terms)
+def polysum_sym(P, variable=None):
+    """
+    returns the polynomial Q in terms of the given variable such
+    that Q(x) = summation(P(i), (i, 0, x)).
+
+    assumes P is a sympy object. If variable == None (the default),
+    then the function will assume it is the single free symbol in P.
+    """
+    if variable == None and len(P.free_symbols) == 1:
+        variable = tuple(P.free_symbols)[0]
+    poly_terms = P.as_poly().as_dict().items()
+    return sum(c * polysum_n(n, variable) for ((n,), c) in poly_terms)
 
 
-def polysum(expr, vtype=DEFAULT_VTYPE, x=None):
-    if type(expr) == int:
-        return polysum_n(expr, vtype, DEFAULT_SYM if x == None else x)
+def polysum(expr, variable=None):
+    """
+    returns the polynomial Q in terms of the given variable such
+    that Q(x) = summation(P(i), (i, 0, x)), where P depends on
+    the given expr:
+        if expr is an sympy object, then it is assumed that P == expr.
+        otherwise, expr is assumed to be an integer n, and P == variable^n
+
+    If variable == None, then the function will try to infer it depending on expr.
+        if expr is an sympy object, then it is assumed that the variable is the single free symbol in expr
+        otherwise, the variable will be DEFAULT_SYM.
+    """
+    if issubclass(type(expr), sp.Basic):
+        return polysum_sym(expr, x)
     else:
-        return polysum_sym(expr, vtype, x)
+        return polysum_n(expr, DEFAULT_SYM if x == None else x)
 
 
---
+++ README.md
@@ -0,0 +1,69 @@
+# PolySum
+
+This implements a new method to find a polynomial equivalent to the sum
+of the `k`th first integers raised to the `n`th power. In other words,
+let `x^n` be the input of this program, then it outputs a polynomial `Q`
+in terms of `x` such that:
+
+```julia
+Q(k) = 1^n + 2^n + 3^n + ... + (k-1)^n + k^n
+```
+
+It's similar to [Schultz's method](https://www.jstor.org/stable/2320260)
+where one obtains a system of equations and solves it to obtain the
+coefficients of the desired polynomial. However, this method was derived
+differently, and obtains an alternative matrix.
+
+# How
+
+Given a Matrix `A` with dimensions `(n+1) x (n+2)` where each element `A[i, j]`<sup>\*</sup>
+  * if `j < i`, then `A[i, j]` is 0.
+  * if `j == n+2`, then `A[i, j]` is `n!/(n - i + 1)!`.
+  * otherwise, `A[i, j]` is `j!/(j - i + 1)!`.
+
+And if we obtain `B`, the reduced row echelon form of `A`,
+then the last column of `B` is a vector `C` of the coefficients
+of the `Q`, where `C[k]` is the coefficient of term `x^k` in `Q(x)`.
+
+The algorithm then is divided into two parts: Making `A`, then
+applying Gauss-Jordan elimination on `A`, which yields the
+coefficients.
+
+\*: with index starting from 1.
+
+# Implementations
+
+There are currently three implementations, each with their
+own set of features and limitations.
+
+## Python
+
+This implementation is meant to be simple and readable, close to
+the algorithm and without any drastic optimizations.
+
+## Julia
+
+Tries to explore as many optimizations as possible while still
+remaining generic. Allows precision to be sacrificed for speed with
+the use of floating-point numbers instead of arbitrary precision
+rationals.
+
+## Rust
+
+Intended to leverage static compilation and the mutability of the
+arbitrary precision numbers to achieve the most performance. Less
+exploratory and meant to show the algorithm in action at its peak.
+
+# Extensions
+
+This algorithm can be extended in two ways:
+
+* to allow different intervals to be used:
+```julia
+Q(k) = i^n + (2i)^n + (3i)^n + ... + ((k-1)*i)^n + (k*i)^n
+```
+* to allow polynomials with two or more terms and with coefficients different from 1
+  to be used as a single input, instead of having to be used separately.
+
+Both of those things can be done outside of the algorithm, but it's worth exploring
+how they would fare integrated into it.
