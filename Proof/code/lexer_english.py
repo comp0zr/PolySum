42ee159bf2f2cb92667ed1cfa06b5ecabf0eaeb3
Fabio Cardeal <ficarde@gmail.com>
2019-03-22 16:38:44 -0300

    spelling and formatting fixes in the proofs

--- Proof/code/lexer_english.py
+++ Proof/code/lexer_english.py
@@ -4,12 +4,12 @@ from pygments.token import *
 class AlgLexer(RegexLexer):
     tokens = {
         'root': [
-            (r'(?i)([\w\-]+)(\()', bygroups(Name.Function, Text)),
+            (r'(?i)([\w\-]+)(\()', bygroups(Name.Function, Name)),
             (r'(?i)(Function|Fill|With|New|For|From|To|Of|End)', Keyword.Reserved),
-            (r'(?i)(\+|\*|:=)', Name.Function),
+            (r'(?i)(\+|\*|-|:=)', Operator),
             (r'(?i)(Matrix(?:es)?|Integers?)', Keyword.Type),
             (r'[0-9]+', Literal),
-            (r'.', Text),
+            (r'.', Name),
         ],
     }
 
--- Proof/code/lexer_portugues.py
+++ Proof/code/lexer_portugues.py
@@ -4,12 +4,12 @@ from pygments.token import *
 class AlgLexer(RegexLexer):
     tokens = {
         'root': [
-            (r'(?i)([\w\-]+)(\()', bygroups(Name.Function, Text)),
+            (r'(?i)([\w\-]+)(\()', bygroups(Name.Function, Name)),
             (r'(?i)(Função|Preencha|com|Nov[oa]|Para|De|Até|Fim)', Keyword.Reserved),
-            (r'(?i)(\+|\*|:=)', Name.Function),
+            (r'(?i)(\+|\*|-|:=)', Operator),
             (r'(?i)(Matriz(?:es)?|Inteiros?)', Keyword.Type),
             (r'[0-9]+', Literal),
-            (r'.', Text),
+            (r'.', Name),
         ],
     }
 
